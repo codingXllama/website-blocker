f7e0a857f35f634a79c76766871f60e4db6b1ec5
codingXllama <osamaalchalabi2@gmail.com>
2020-01-03 13:26:47 -0500

    created an alt-host file followed by backbone time checking logic

--- app.py
+++ app.py
@@ -9,17 +9,34 @@ The Host file on Windows is located in: C:\Windows\System32\drivers\etc
 import time
 from datetime import datetime as dt
 
+
+# Creating a temp host file, that we can alter and then use it to override the systems original host file
+temp_HostFile = "hosts"
+
 # creating a raw string via \\ or using 'r' letter infront of the string
 windows_Hostpath = r"C:\Windows\System32\drivers\etc"
+
+# The the required the redirection file that will be used to block desired sites
 redirection_IP_route = "127.0.0.1"
 
+
 websites_toBlock = ["www.gmail.com", "gmail.com"]
-currentTime = dt(dt.now().year, dt.now().month, dt.now().day, 8)
-timeToBlock = dt(dt.now().year, dt.now().month, dt.now().day, 10)
+currentTime = dt(
+    dt.now().year, dt.now().month, dt.now().day, dt.now().hour, dt.now().minute
+)
+timeToBlock = dt(dt.now().year, dt.now().month, dt.now().day, dt.now().hour, 18)
 
+while True:
 
-if currentTime == timeToBlock:
-    print("time to block")
-else:
-    print("no block")
+    # checking the status of the time in regards the hours (present vs blocking time hours)
+    if dt(
+        dt.now().year, dt.now().month, dt.now().day, dt.now().hour, dt.now().minute
+    ) == dt(dt.now().year, dt.now().month, dt.now().day, dt.now().hour, 24):
+        print("time to block")
+    else:
+        print("no block")
+
+    # The program will now sleep for n-seconds, before rechecking the time
+    print("time to sleep")
+    time.sleep(2)
 
