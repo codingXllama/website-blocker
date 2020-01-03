139b6f5c752a09f78f45823a1913ff45e95f380e
codingXllama <osamaalchalabi2@gmail.com>
2020-01-03 12:53:39 -0500

    initialized the variables for current time and block time using datetime library

--- app.py
+++ app.py
@@ -5,9 +5,21 @@ The Host file on mac is located in: /etc/hosts
 The Host file on Windows is located in: C:\Windows\System32\drivers\etc
 
 """
+
+import time
+from datetime import datetime as dt
+
 # creating a raw string via \\ or using 'r' letter infront of the string
 windows_Hostpath = r"C:\Windows\System32\drivers\etc"
-redirection_IpRoute = "127.0.0.1"
+redirection_IP_route = "127.0.0.1"
 
 websites_toBlock = ["www.gmail.com", "gmail.com"]
+currentTime = dt(dt.now().year, dt.now().month, dt.now().day, 8)
+timeToBlock = dt(dt.now().year, dt.now().month, dt.now().day, 10)
+
+
+if currentTime == timeToBlock:
+    print("time to block")
+else:
+    print("no block")
 
