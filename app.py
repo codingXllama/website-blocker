b6795eff9d49f6359eb0722d9e530dea57597c2c
codingXllama <osamaalchalabi2@gmail.com>
2020-01-03 11:55:06 -0500

    initialized required host locations

---
+++ app.py
@@ -0,0 +1,13 @@
+# Web blocker python program
+"""
+
+The Host file on mac is located in: /etc/hosts
+The Host file on Windows is located in: C:\Windows\System32\drivers\etc
+
+"""
+# creating a raw string via \\ or using 'r' letter infront of the string
+windows_Hostpath = r"C:\Windows\System32\drivers\etc"
+redirection_IpRoute = "127.0.0.1"
+
+websites_toBlock = ["www.gmail.com", "gmail.com"]
+
