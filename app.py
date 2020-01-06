823e2d1ce6b42fd8c5f8d77a5d7e65f90b94bafe
codingXllama <osamaalchalabi2@gmail.com>
2020-01-06 15:06:07 -0500

    mades changes in the else block such that the program will not duplicate the host fille

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from datetime import datetime as dt
 temp_HostFile = "hosts"
 
 # creating a raw string via \\ or using 'r' letter infront of the string
-windows_Hostpath = r"C:\Windows\System32\drivers\etc"
+windows_Hostpath = "C:\Windows\System32\drivers\etc"
 
 # The the required the redirection file that will be used to block desired sites
 redirection_IP_route = "127.0.0.1"
@@ -32,20 +32,36 @@ while True:
     # checking the status of the time in regards the hours (present vs blocking time hours)
     if dt(dt.now().year, dt.now().month, dt.now().day, dt.now().hour, startingMinutes ) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, dt.now().hour, endingMinutes):
         print("time to block")
-        with open(temp_HostFile,'r+') as tempHostFile:
-            hostFileContent=tempHostFile.read()
+        with open(temp_HostFile,'r+') as myFile:
+            hostFileContent=myFile.read()
             for website in websites_toBlock:
                 if website in hostFileContent:
                     pass
                 else:
-                    tempHostFile.write(f"{redirection_IP_route} {website}\n")
+                    myFile.write(f"{redirection_IP_route} {website}\n")
+    
+    
                     
-    else:                   
-        with open(tempHostFile,"r+") as file:
-            hostFileContent=file.readlines()
+    else: 
+        '''The purpose of this else block is to open the host files
+         and then write the required files to be blocked, if they already exist,
+          then we don't write them again
+        '''
+        with open(temp_HostFile,"r+") as myFile:
+            
+            #opening the already existing host file and then storing each line in  a variable called ' hostFileContent'
+            hostFileContent=myFile.readlines()
+            
+              # placing the file pointer just before the start of the file content
+            myFile.seek(0)
             for line in hostFileContent:
+                # if the website in the lines we read is not stored in the websites to block list, then we will write that line to the host file 
                 if not any(website in line for website in websites_toBlock):
-                    file.write(line)
+                    myFile.write(line)
+                    
+            # cutting out all the written text after the end of file
+            # basically it's purpose here is to revert the changes that has happened to the file (writting to the file)
+            myFile.truncate()
     # The program will now sleep for n-seconds, before rechecking the time
         print("Fun hours....")
     time.sleep(2)
