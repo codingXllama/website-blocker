ff6af69d8659d235d5d086f9c49d62636af3124b
codingXllama <osamaalchalabi2@gmail.com>
2020-01-06 12:35:36 -0500

    created the ability to modify the host files

--- app.py
+++ app.py
@@ -20,16 +20,26 @@ windows_Hostpath = r"C:\Windows\System32\drivers\etc"
 redirection_IP_route = "127.0.0.1"
 
 
-websites_toBlock = ["www.gmail.com", "gmail.com"]
-currentTime = dt(
-    dt.now().year, dt.now().month, dt.now().day, dt.now().hour, dt.now().minute
-)
-timeToBlock = dt(dt.now().year, dt.now().month, dt.now().day, dt.now().hour, 18)
+websites_toBlock = ["https://mail.google.com/", "www.gmail.com"]
+# currentTime = dt(
+#     dt.now().year, dt.now().month, dt.now().day, dt.now().hour, dt.now().minute
+# timeToBlock = dt(dt.now().year, dt.now().month, dt.now().day, dt.now().hour, 18)
+
+startingMinutes=int(input("Enter the starting minutes: "))
+endingMinutes=int(input("Enter the Ending minutes: "))
 while True:
 
     # checking the status of the time in regards the hours (present vs blocking time hours)
-    if dt(dt.now().year, dt.now().month, dt.now().day, dt.now().hour, 2 )<dt.now()< dt(dt.now().year, dt.now().month, dt.now().day, dt.now().hour, 4):
+    if dt(dt.now().year, dt.now().month, dt.now().day, dt.now().hour, startingMinutes ) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, dt.now().hour, endingMinutes):
         print("time to block")
+        with open(temp_HostFile,'r+') as tempHostFile:
+            hostFileContent=tempHostFile.read()
+            for website in websites_toBlock:
+                if website in hostFileContent:
+                    pass
+                else:
+                    tempHostFile.write(f"{redirection_IP_route} {website}\n")
+                    
     else:                   
         print("no block")
     # The program will now sleep for n-seconds, before rechecking the time
